c736a0450ff55f44b04877664ac4f4b8dcbf515d
irsyadpahlapi <irsyadpahlapi28@gmail.com>
2018-04-12 14:27:03 +0700

    tinggal notification

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -4,24 +4,44 @@
       <div class="row">
         <div class="col-md-6">
           <div class="jumbotron">
-            <h1>registration</h1>
+            <h1 style="text-align:center;">registration</h1>
             <div class="form-group">
-              <label for="exampleFormControlInput1">Email address</label>
-              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+              <label for="exampleFormControlInput1">Email address
+                <span v-if="addpassword.length <= 6 && addpassword.length !== 0" class="notif">email tidak valid</span>
+              </label>
+              <input type="email" class="form-control" v-model="addemail" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="form-group">
+              <label for="password">Password
+                <span v-if="addpassword.length <= 6 && addpassword.length !== 0" class="notif">password tidak boleh kurang dari 6</span>
+              </label>
+              <input type="password" class="form-control" v-model="addpassword" id="password" placeholder="masukan password">
+            </div>
+            <div class="form-group">
+              <label for="repassword">Repasword</label>
+              <input type="password" class="form-control" v-model="readdpassword" id="repassword" placeholder="ulangi password">
             </div>
 
+            <button type="button" name="button" class="btn btn-primary" @click="register">Register</button>
           </div>
 
         </div>
 
         <div class="col-md-6">
           <div class="jumbotron">
-            <h1>Login</h1>
+            <h1 style="text-align:center;">Login</h1>
             <div class="form-group">
-              <label for="exampleFormControlInput1" style="text-align:none;">Email address</label>
-              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+              <label for="exampleFormControlInput1">Email</label>
+              <input type="email" v-model="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
             </div>
-
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" v-model="password" id="password" placeholder="masukan password">
+            </div>
+            <button type="button" name="button" class="btn btn-primary" @click="login">Login</button>
+            <div class="col-md-12">
+              <p v-show="token" class="lead">{{token}}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -31,11 +51,40 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      addemail: '',
+      addpassword: '',
+      readdpassword: '',
+      email: '',
+      password: '',
+      token: ''
+    }
+  },
+  methods: {
+    register () {
+      axios.post('http://localhost:3000/signup/', {
+        email: this.addemail,
+        password: this.addpassword
+      }).then(response => {
+        this.addemail = ''
+        this.addpassword = ''
+        this.readdpassword = ''
+      })
+    },
+    login () {
+      axios.post('http://localhost:3000/signin/', {
+        email: this.email,
+        password: this.password
+      }).then(response => {
+        this.email = ''
+        this.password = ''
+        this.token = response.data.token
+      })
     }
   }
 }
@@ -43,18 +92,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.notif{
+  color: red;
 }
 </style>
